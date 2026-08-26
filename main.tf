c24fa3b92a3d291ea7582f3986a5bbc88a466896
sareddy naveenkumarreddy <naveensareddy2003@gmail.com>
2026-08-26 16:11:03 +0530

    Update AWS region and instance configurations

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-0b6d9d3d33ba97d99"
-  instance_type          = "c7i-flex.large"
-  key_name               = "winscp"
-  vpc_security_group_ids = ["sg-0a6da09a007dadc56"]
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "m7i-flex.12xlarge
+"
+  key_name               = "server"
+  vpc_security_group_ids = ["sg-0b7bcdd2627b4101c"]
   tags = {
     Name = var.instance_names[count.index]
   }
@@ -17,8 +18,8 @@ variable "instance_names" {
   default = ["jenkins", "tomcat-1", "Monitoring server"]
 }
 
-resource "aws_s3_bucket" "two" {
-  bucket = "kumar-bucket-283"
+resource "aws_s3_bucket" "one" {
+  bucket = "picece-bucket-3445"
   versioning {
     enabled = true
   }
